d1f868e3ec714cfef20920615c19e90d31c84136
Levan <kurhulilevan321@gmail.com>
2021-01-18 23:45:16 +0300

    first one

---
+++ script.js
@@ -0,0 +1,12 @@
+function validatePIN (pin) {
+  debugger;
+  if (pin.length !== 4 || pin.length !== 6) {
+    return false;
+  }
+  
+  for (let i = 0; i < pin.length; i++) {
+    if (pin[i] === '-' || pin[i] === '.' || isNaN(+pin[i]) ){
+      return false;
+    } else return true;
+  }
+}
